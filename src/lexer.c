a66d127e63bdcf066d670bd3157736c5656f5194
Texugo <texugo13@duck.com>
2026-06-06 22:49:00 -0300

    extended some things

--- src/lexer.c
+++ src/lexer.c
@@ -47,100 +47,87 @@ token* tokenize(char* string){
    for(int i = 0; i <= strlen(string); i++){
 
       bool nmbr = string[i] == 'n' && string[i + 1] == 'm' && string[i + 2] == 'b' && string[i + 3] == 'r' && string[i + 4] == ' ';
-      bool boolean = string[i] == 'b' && string[i + 1] == 'o' && string[i + 2] == 'o' && string[i + 3] == 'l' && string[i + 4] == ' ';
+      bool isBool = string[i] == 'b' && string[i + 1] == 'o' && string[i + 2] == 'o' && string[i + 3] == 'l' && string[i + 4] == ' ';
       bool vec2 = string[i] == 'v' && string[i + 1] == 'e' && string[i + 2] == 'c' && string[i + 3] == '2' && string[i + 4] == ' ';
-      bool strg = string[i] == 's' && string[i + 1] == 't' && string[i + 2] == 'r' && string[i + 3] == 'n' && string[i + 4] == 'g' && string[i + 5] == ' ';
+      bool isString = string[i] == 's' && string[i + 1] == 't' && string[i + 2] == 'r' && string[i + 3] == 'n' && string[i + 4] == 'g' && string[i + 5] == ' ';
       bool fn = string[i] == 'f' && string[i + 1] == 'n' && string[i + 2] == ' ';
-      bool isBoolean = string[i] == 't' && string[i + 1] == 'r' && string[i + 2] == 'u' && string[i + 3] == 'e' ||
+      bool isBoolValue = string[i] == 't' && string[i + 1] == 'r' && string[i + 2] == 'u' && string[i + 3] == 'e' ||
                        string[i] == 'f' && string[i + 1] == 'a' && string[i + 2] == 'l' && string[i + 3] == 's' && string[i + 4] == 'e';
 
       if(string[i] == ' '){
-         continue;;
+         continue;
+      }
+      else if(string[i] == '#'){
+         break;
       }
       else if(needleInHaystack(string[i], "(){}[]")){
-         // printf("PARENTHESES: %c\n", string[i]);
          char token_str[2] = {string[i], '\0'};
          appendToken(tokens, PARENTHESES, token_str);
       }
       else if(needleInHaystack(string[i], "+-/*=!^")){
-         // printf("OPERATOR: %c\n", string[i]);
          char token_str[2] = {string[i], '\0'};
          appendToken(tokens, OPERATOR, token_str);
       }
       else if(needleInHaystack(string[i], "0123456789.,")){
-         // printf("NUMBER: %c\n", string[i]);
-         for(int j = i + 1; j < strlen(string); j++){
-            printf("%c\n", string[j]);
-            if(!needleInHaystack(string[j], "0123456789.,")){
-               j++;
-               char numberLiberal[j - i];
+         int j = i;
 
-               for(int k = 0;k <= j - i; k++){
-                  numberLiberal[k] = string[k + i];
-               }
-               appendToken(tokens, NUMBER, numberLiberal);
-               i = j; 
-               break;
-            }
+         for(; j <= strlen(string) - 1; j++){ if(!needleInHaystack(string[j], "0123456789.")){j--; break;}}
+
+         char numberLiteral[0];
+         for(int k = i; k <= j; k++){
+            numberLiteral[k - i] = string[k];
          }
-         char token_str[2] = {string[i], '\0'};
-         appendToken(tokens, NUMBER, token_str);
+         appendToken(tokens, NUMBER, numberLiteral);
+         i = j;
       }
-      else if(isBoolean){
+      else if(isBoolValue){
          char trueKeyword[5] = {string[i], string[i + 1], string[i + 2], string[i + 3]};
          char falseKeyword[6] = {string[i], string[i + 1], string[i + 2], string[i + 3], string[i + 4]};
 
          if(string[i] == 'f' && string[i + 1] == 'a' && string[i + 2] == 'l' && string[i + 3] == 's' && string[i + 4] == 'e'){
-            // printf("KEYWORD: %s\n", falseKeyword);
             appendToken(tokens, KEYWORD, falseKeyword);
             continue;
          }
-         // printf("KEYWORD: %s\n", trueKeyword);
          appendToken(tokens, KEYWORD, trueKeyword);
       }
-      else if(nmbr || boolean || vec2){
+      else if(nmbr || isBool || vec2){
          char variable[5] = {string[i], string[i + 1], string[i + 2], string[i + 3]};
-         // printf("VARIABLE: %s\n", variable);
          appendToken(tokens, VARIABLE, variable);
          i += 4;
       }
-      else if(strg){
+      else if(isString){
          char variable[6] = {string[i], string[i + 1], string[i + 2], string[i + 3], string[i + 4]};
-         // printf("VARIABLE: %s\n", variable);
          appendToken(tokens, VARIABLE, variable);
          i += 5;
       }
       else if(fn){
          char variable[6] = {string[i], string[i + 1], string[i + 2]};
-         // printf("VARIABLE: %s\n", variable);
          appendToken(tokens, VARIABLE, variable);
          i += 2;
       }
       else if(string[i] == '\"'){
-         for(int j = i + 1; j < strlen(string); j++){
-            printf("%c\n", string[j]);
-            if(string[j] == '\"'){
-               j++;
-               char stringLiberal[j - i];
+         int j = i + 1;
 
-               for(int k = 0;k <= j - i; k++){
-                  stringLiberal[k] = string[k + i];
-               }
-               appendToken(tokens, STRING, stringLiberal);
-               i = j;
-               break;
-            }
+         for(; j <= strlen(string) - 1; j++){ if(string[j] == '\"'){break;}}
+
+         char stringLiteral[0];
+         for(int k = i; k <= j; k++){
+            stringLiteral[k - i] = string[k];
          }
+
+         stringLiteral[j - i + 1] = '\0';
+         appendToken(tokens, STRING, stringLiteral);
+         i = j;
       }
       else if(needleInHaystack(string[i], "\n\t")){
          continue;
       }
       else{
-         printf("%c\n", string[i]);
+         
       }
    }
    for(int i = 0; i < MAX_TOKENS; i++){
-      if(tokens[i].token) printf("%s ", tokens[i].token);
+      if(tokens[i].token) printf("%d->'%s'", tokens[i].type, tokens[i].token);
    }
    printf("\n");
 
